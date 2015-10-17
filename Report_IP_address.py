81879cc8ee1f0cbba6f9cea693f73d968d27dbd9
yueyangming <yueyangming1024@sina.com>
2015-10-17 16:38:20 +0800

    Version 1.3
    Can get Internet Ip address using re part now.

--- Report_IP_address.py
+++ Report_IP_address.py
@@ -7,13 +7,17 @@ import struct
 from instapush import Instapush, App
 import urllib2
 import time
+import re
 
 # Initialization
 
 Exist_wlan = 0
 Exist_Eth = 0
+string_internet = 'Internet address not exist, werid, check it'
 
 #Test if the Raspberry can connect the internet
+
+# def check_internet():
 
 Url_instapush = 'https://instapush.im/'
 while 1:
@@ -40,18 +44,36 @@ try:
 except:
     Ip_address_wlan0 = 'Not exist'
 
+# Get internet Ip address
+try:
+    pattern_for_ip = re.compile('(\d{1,3}\.){3}\d{1,3}')
+    url_get_internet_address = "http://www.ip138.com/ip2city.asp"
+    opener = urllib2.urlopen(url_get_internet_address)
+    string_ip = opener.read()
+    Ip_address_internet = re.search(pattern_for_ip,string_ip).group()
+    string_internet = 'Internet Ip address : ' + Ip_address_internet
+except:
+    pass
+
 string_Eth0 = 'Eth Ip address : ' + Ip_address_Eth0
-string_wlan0 = 'Wlan Ip address: ' + Ip_address_wlan0
+string_wlan0 = 'Wlan Ip address : ' + Ip_address_wlan0
 
 # Push information to Mobile phone via Instapush
 
 app = App(appid = '561bac12a4c48a31793792b5', secret = '5b3446093d50511955f95b589988c541')
 
 if Exist_wlan:
-     app.notify(event_name = 'Report_IP_address_of_raspberry_pi', trackers ={'Message': string_wlan0})
+    app.notify(event_name = 'Report_IP_address_of_raspberry_pi', trackers ={'Message': string_wlan0})
 
 if Exist_Eth:
     app.notify(event_name = 'Report_IP_address_of_raspberry_pi', trackers ={'Message': string_Eth0})
+
+try:
+    app.notify(event_name = 'Report_IP_address_of_raspberry_pi', trackers ={'Message': string_internet})
+except :
+    pass
+
+
 
 # Application ID :
 # 561bac12a4c48a31793792b5
