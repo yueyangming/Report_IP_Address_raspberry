0a0a8aa95bc22cb9d8f0a2a5234559d8ccd6d28d
yueyangming <yueyangming1024@sina.com>
2015-10-17 12:58:28 +0800

    Version 1.1
    Add try-except part, So when raspberry pi only connect with wlan or eth
    , There is no mistake and the program will run .

--- Report_IP_address.py
+++ Report_IP_address.py
@@ -6,24 +6,37 @@ import fcntl
 import struct
 from instapush import Instapush, App
 
+# Initialization
+
+Exist_wlan = 0
+Exist_Eth = 0
+
 # Get IP address
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+try:
+    Ip_address_Eth0 = socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', 'eth0'))[20:24])
+    Exist_Eth = 1
+except:
+    Ip_address_Eth0 = 'Not exist'
 
-Ip_address_Eth0 = socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', 'eth0'))[20:24])
-
-Ip_address_wlan0 = socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', 'wlan0'))[20:24])
+try:
+    Ip_address_wlan0 = socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', 'wlan0'))[20:24])
+    Exist_wlan = 1
+except:
+    Ip_address_wlan0 = 'Not exist'
 
 string_Eth0 = 'Eth Ip address : ' + Ip_address_Eth0
 string_wlan0 = 'Wlan Ip address: ' + Ip_address_wlan0
 
-# Push information to App
+# Push information to Mobile phone via Instapush
 
 app = App(appid = '561bac12a4c48a31793792b5', secret = '5b3446093d50511955f95b589988c541')
 
-app.notify(event_name = 'Report_IP_address_of_raspberry_pi', trackers ={'Message': string_Eth0})
+if Exist_wlan:
+     app.notify(event_name = 'Report_IP_address_of_raspberry_pi', trackers ={'Message': string_wlan0})
 
-app.notify(event_name = 'Report_IP_address_of_raspberry_pi', trackers ={'Message': string_wlan0})
-
+if Exist_Eth:
+    app.notify(event_name = 'Report_IP_address_of_raspberry_pi', trackers ={'Message': string_Eth0})
 
 # Application ID :
 # 561bac12a4c48a31793792b5
